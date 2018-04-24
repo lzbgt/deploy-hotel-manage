c84a9dc25dea4ffe109b63c5e889ed129b61d68c
Bruce.Lu <lzbgt@126.com>
2018-04-24 15:08:19 +0800

    init

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+ver=1.0
+proj_base=/Users/Bruce/work/dfws/wkxhotel-parent/
+docker_registry=docker-registry.cluster.dfwsgroup.cn/library
+
+
+declare -a dir_services=("wkxhotel-eureka-server" "wkxhotel-file" "wkxhotel-redis" "wkxhotel-service-job" "wkxhotel-service-statistics" "wkxhotel-service-gateway" "wkxhotel-web-customer" "wkxhotel-web-waiter" "wkxhotel-company-manage" "wkxhotel-dfws-manage")
+for i in "${dir_services[@]}"
+do
+  echo "building $i"
+  cd $proj_base/$i
+  mvn docker:build
+  docker push $docker_registry/$i:$ver
+done
