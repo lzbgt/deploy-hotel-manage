215605483800623f0dbf405fb4bb93303a5d694b
Bruce.Lu <lzbgt@126.com>
2018-04-26 15:33:56 +0800

    init

--- build.sh
+++ build.sh
@@ -1,16 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 ver=1.0
-proj_base=/Users/Bruce/work/dfws/wkxhotel-parent/
+proj_base=/Users/Bruce/work/dfws/wkxhotel-parent
 docker_registry=docker-registry.cluster.dfwsgroup.cn/library
 
+cd $proj_base
+mvn clean install package -DskipTests
 
 declare -a dir_services=("wkxhotel-eureka-server" "wkxhotel-file" "wkxhotel-redis" "wkxhotel-service-job" "wkxhotel-service-statistics" "wkxhotel-service-gateway" "wkxhotel-web-customer" "wkxhotel-web-waiter" "wkxhotel-company-manage" "wkxhotel-dfws-manage")
 for i in "${dir_services[@]}"
 do
   echo "building $i"
   cd $proj_base/$i
-  mvn docker:build
+  mvn docker:build -DskipTests
   docker push $docker_registry/$i:$ver
 done
